a3962d359accbc5cb1c4cd6ef297b578b29f4e46
Kyle Corry <kylecorry31@gmail.com>
2025-02-11 20:40:30 -0500

    Add edit text for state

--- app/src/main/java/com/kylecorry/bell/ui/components/Alerts.kt
+++ app/src/main/java/com/kylecorry/bell/ui/components/Alerts.kt
@@ -1,5 +1,6 @@
 package com.kylecorry.bell.ui.components
 
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.ViewGroup
 import com.kylecorry.andromeda.core.ui.useCallback
@@ -8,10 +9,13 @@ import com.kylecorry.andromeda.fragments.inBackground
 import com.kylecorry.andromeda.fragments.useBackgroundEffect
 import com.kylecorry.andromeda.views.reactivity.AndromedaViews.Column
 import com.kylecorry.andromeda.views.reactivity.AndromedaViews.Component
+import com.kylecorry.andromeda.views.reactivity.AndromedaViews.EditText
+import com.kylecorry.andromeda.views.reactivity.AndromedaViews.Row
 import com.kylecorry.andromeda.views.reactivity.AndromedaViews.Text
 import com.kylecorry.bell.domain.Alert
 import com.kylecorry.bell.infrastructure.alerts.AlertUpdater
 import com.kylecorry.bell.infrastructure.persistence.AlertRepo
+import com.kylecorry.bell.infrastructure.persistence.UserPreferences
 
 
 fun Alerts() = Component {
@@ -26,6 +30,10 @@ fun Alerts() = Component {
 
     val repo = useMemo(context) {
         AlertRepo.getInstance(context)
+    }
+
+    val preferences = useMemo(context) {
+        UserPreferences(context)
     }
 
     // Load the initial alerts
@@ -58,28 +66,55 @@ fun Alerts() = Component {
     }
 
     val dp16 = useSizeDp(16f).toInt()
+    val dp48 = useSizeDp(48f).toInt()
 
-    Column(if (loading) {
-        Text {
-            text = "Loading $loadingMessage ${(progress * 100).toInt()}%"
+    val onStateTextChanged = useMemo {
+        object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                preferences.state = s.toString()
+            }
+
+            override fun afterTextChanged(s: android.text.Editable?) {
+            }
+        }
+    }
+
+    Column(
+        Row({
             marginTop = dp16
             marginStart = dp16
             marginEnd = dp16
             marginBottom = dp16
             layoutGravity = Gravity.CENTER_HORIZONTAL
             width = ViewGroup.LayoutParams.WRAP_CONTENT
-        }
-    } else {
-        UpdateButton {
-            onUpdate = updateAlerts
-            marginTop = dp16
-            marginStart = dp16
-            marginEnd = dp16
-            marginBottom = dp16
-            layoutGravity = Gravity.CENTER_HORIZONTAL
-            width = ViewGroup.LayoutParams.WRAP_CONTENT
-        }
-    },
+        },
+            EditText {
+                text = preferences.state
+                updateText = true
+                hint = "State Code"
+                onTextChanged = onStateTextChanged
+                marginEnd = dp16
+                width = ViewGroup.LayoutParams.WRAP_CONTENT
+                layoutGravity = Gravity.CENTER
+                height = dp48
+            },
+            if (loading) {
+                Text {
+                    text = "Loading $loadingMessage ${(progress * 100).toInt()}%"
+                    width = ViewGroup.LayoutParams.WRAP_CONTENT
+                    layoutGravity = Gravity.CENTER
+                }
+            } else {
+                UpdateButton {
+                    onUpdate = updateAlerts
+                    width = ViewGroup.LayoutParams.WRAP_CONTENT
+                    layoutGravity = Gravity.CENTER
+                }
+            }
+        ),
         AlertList {
             this.alerts = alerts
             onDelete = deleteAlert
