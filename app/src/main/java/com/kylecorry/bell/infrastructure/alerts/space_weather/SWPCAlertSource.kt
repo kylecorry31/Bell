aaf7a3bd2a6a6787136f26d3e14f2bcecf8ebc83
Kyle Corry <kylecorry31@gmail.com>
2025-02-14 07:54:30 -0500

    Process more SWPC alerts

--- app/src/main/java/com/kylecorry/bell/infrastructure/alerts/space_weather/SWPCAlertSource.kt
+++ app/src/main/java/com/kylecorry/bell/infrastructure/alerts/space_weather/SWPCAlertSource.kt
@@ -19,8 +19,12 @@ import java.time.ZoneId
 class SWPCAlertSource(context: Context) : AlertSource {
 
     private val titleRegex = Regex("(WARNING|ALERT|SUMMARY|WATCH):\\s(.*)")
+
+    // TODO: Capture the storm level
     private val stormDateRegex = Regex("([A-Z][a-z]{2}\\s\\d{2}):")
     private val messageCodeRegex = Regex("Space Weather Message Code: (\\w+)")
+    private val validToRegex = Regex("Valid To: (.*)")
+    private val validFromRegex = Regex("Valid From: (.*)")
 
     private val loader = AlertLoader(context)
 
@@ -48,6 +52,18 @@ class SWPCAlertSource(context: Context) : AlertSource {
         "WATA99" to "4+"
     )
 
+    private fun isWatch(code: String): Boolean {
+        return code.startsWith("WAT")
+    }
+
+    private fun isWarning(code: String): Boolean {
+        return code.startsWith("WAR")
+    }
+
+    private fun isAlert(code: String): Boolean {
+        return code.startsWith("ALT")
+    }
+
     override suspend fun load(): List<Alert> {
         val rawAlerts = loader.load(
             FileType.JSON,
@@ -64,49 +80,84 @@ class SWPCAlertSource(context: Context) : AlertSource {
             val sent = DateTimeParser.parseInstant(it["sent"]?.replace(" ", "T") + "Z")
                 ?: return@mapNotNull null
 
-            // TODO: Handle other types of alerts
-            if (!description.contains("WATCH: Geomagnetic Storm Category")) {
-                return@mapNotNull null
-            }
+//            // TODO: Handle other types of alerts
+//            if (!description.contains("WATCH: Geomagnetic Storm Category")) {
+//                return@mapNotNull null
+//            }
 
             val messageCode = messageCodeRegex.find(description)?.groupValues?.get(1)
                 ?: return@mapNotNull null
+
+            val stormLevel = codeToStormLevel[messageCode] ?: return@mapNotNull null
+
             val title = titleRegex.find(description)?.groupValues?.get(2)
                 ?: return@mapNotNull null
 
             val dates = stormDateRegex.findAll(description).toList()
 
-            val stormLevel = codeToStormLevel[messageCode] ?: return@mapNotNull null
 
-            val expirationDate = dates.flatMap {
-                listOf(
-                    "${it.groupValues[1]} ${sent.atZone(ZoneId.of("UTC")).year}",
-                    "${it.groupValues[1]} ${sent.atZone(ZoneId.of("UTC")).year + 1}"
-                )
-            }.mapNotNull {
-                DateTimeParser.parse(it, ZoneId.of("UTC"))?.atEndOfDay()?.toInstant()
-            }.filter { Duration.between(sent, it).abs() < Duration.ofDays(14) }
-                .maxOrNull()
+            val expirationDate = if (isWatch(messageCode)) {
+                dates.flatMap {
+                    listOf(
+                        "${it.groupValues[1]} ${sent.atZone(ZoneId.of("UTC")).year}",
+                        "${it.groupValues[1]} ${sent.atZone(ZoneId.of("UTC")).year + 1}"
+                    )
+                }.mapNotNull {
+                    DateTimeParser.parse(it, ZoneId.of("UTC"))?.atEndOfDay()?.toInstant()
+                }.filter { Duration.between(sent, it).abs() < Duration.ofDays(14) }
+                    .maxOrNull()
+            } else {
+                validToRegex.find(description)?.groupValues?.get(1)?.let {
+                    DateTimeParser.parseInstant(it)
+                }
+            }
+
+            val effectiveDate = validFromRegex.find(description)?.groupValues?.get(1)?.let {
+                DateTimeParser.parseInstant(it)
+            }
+
+            val label = if (isWatch(messageCode)) {
+                "Watch"
+            } else if (isWarning(messageCode)) {
+                "Warning"
+            } else {
+                "Alert"
+            }
+
+            val identifier = if (isWatch(messageCode)) {
+                "geomagnetic-storm-watch"
+            } else {
+                messageCode.replace("ALT", "").replace("WAR", "")
+            }
+
+            val certainty = if (isWatch(messageCode)) {
+                Certainty.Possible
+            } else if (isWarning(messageCode)) {
+                Certainty.Likely
+            } else {
+                Certainty.Observed
+            }
 
 
-            // TODO: Handle cancelations
+            // TODO: Handle cancellations
             Alert(
                 id = 0,
-                identifier = "geomagnetic-storm-warning",
+                identifier = identifier,
                 sender = "SWPC",
                 sent = sent,
                 source = getUUID(),
                 category = Category.Infrastructure,
-                event = "Geomagnetic Storm Watch (G$stormLevel)",
+                event = "Geomagnetic Storm $label (G$stormLevel)",
                 headline = title,
                 urgency = Urgency.Unknown,
                 severity = codeToSeverity[messageCode] ?: Severity.Minor,
-                certainty = Certainty.Unknown,
+                certainty = certainty,
                 description = description,
+                effective = effectiveDate,
                 expires = expirationDate
-                    ?: sent.plus(Duration.ofDays(Constants.DEFAULT_EXPIRATION_DAYS)),
+                    ?: sent.plus(Duration.ofDays(1)),
             )
-        }.sortedByDescending { it.sent }.take(1)
+        }.sortedByDescending { it.sent }
     }
 
     override fun getUUID(): String {
